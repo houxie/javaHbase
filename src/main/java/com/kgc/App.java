5975228d42234138d19179576ef1bb6cde89b81b
houxie <1106581733@qq.com>
2019-07-05 11:04:19 +0800

    HBaseApi

--- src/main/java/com/kgc/App.java
+++ src/main/java/com/kgc/App.java
@@ -5,14 +5,14 @@ package com.kgc;
  */
 public class App {
     public static void main(String[] args) {
-//      JavaHbaseAPI javaHbaseAPI = new JavaHbaseAPI();
+      JavaHbaseAPI javaHbaseAPI = new JavaHbaseAPI();
 //      javaHbaseAPI.createTable();
 //      javaHbaseAPI.deleteTable();
 //      javaHbaseAPI.listTables();
 //      javaHbaseAPI.insertResouce();
 //      javaHbaseAPI.getResource();
 //      javaHbaseAPI.deleteData();
-//      javaHbaseAPI.scanData();
+      javaHbaseAPI.scanData();
 
 
     }
